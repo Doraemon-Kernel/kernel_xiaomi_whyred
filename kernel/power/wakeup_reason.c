5312537fedd23f08ab1e6c6a819640b3d4b24ec1
Wei Wang <wvw@google.com>
2016-10-27 11:03:12 -0700

    power: wakeup_reason: make log function work in interrupt context
    If log_suspend_abort_reason called in interrupt context, the spinlock
    may deadlock.
    With https://lkml.org/lkml/2014/9/1/404, wakeup mechanism changed, so
    log_suspend_abort_reason can be placed in interrupt context to retrieve
    the abort reasons.
    
    Bug: 32371978
    Change-Id: I6902770e54b663d21b47289daec19401fc0dbed4
    Signed-off-by: Wei Wang <wvw@google.com>

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -451,7 +451,7 @@ void log_suspend_abort_reason(const char *fmt, ...)
 	vsnprintf(abort_reason, MAX_SUSPEND_ABORT_LEN, fmt, args);
 	va_end(args);
 
-	spin_unlock(&resume_reason_lock);
+	spin_unlock_irqrestore(&resume_reason_lock, flags);
 }
 
 static bool match_node(struct wakeup_irq_node *n, void *_p)
@@ -463,9 +463,10 @@ static bool match_node(struct wakeup_irq_node *n, void *_p)
 int check_wakeup_reason(int irq)
 {
 	bool found;
-	spin_lock(&resume_reason_lock);
+	unsigned long flags;
+	spin_lock_irqsave(&resume_reason_lock, flags);
 	found = !walk_irq_node_tree(base_irq_nodes, match_node, &irq);
-	spin_unlock(&resume_reason_lock);
+	spin_unlock_irqrestore(&resume_reason_lock, flags);
 	return found;
 }
 
@@ -548,7 +549,7 @@ static int wakeup_reason_pm_event(struct notifier_block *notifier,
 	unsigned long flags;
 	switch (pm_event) {
 	case PM_SUSPEND_PREPARE:
-		spin_lock(&resume_reason_lock);
+		spin_lock_irqsave(&resume_reason_lock, flags);
 		suspend_abort = false;
 		spin_unlock_irqrestore(&resume_reason_lock, flags);
 		/* monotonic time since boot */
