440076ac8b69cc886fd3fddb16a0b403d2220ae9
Vikram Mulukutla <markivx@codeaurora.org>
2019-07-16 16:03:06 -0300

    sched: walt: Update cpufreq on window rollover
    Relying on the PELT-path callsite(s) of cpufreq_update_util
    may not be a good idea since we may miss updating schedutil
    with the latest WALT load for a tick period or more if the
    CPU goes idle. Add a call to cpufreq_update_util just after
    we roll over a WALT window.
    
    Change-Id: I995e85b05c912d9f00c4d83f9a8ad499a3edec93
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>
    (cherry picked from commit d9e22eb38a622d7b72544e747ab375e7672211fb)
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -217,8 +217,9 @@ update_window_start(struct rq *rq, u64 wallclock)
 
 	nr_windows = div64_u64(delta, walt_ravg_window);
 	rq->window_start += (u64)nr_windows * (u64)walt_ravg_window;
-
 	rq->cum_window_demand = rq->cumulative_runnable_avg;
+
+	cpufreq_update_util(rq, 0);
 }
 
 /*
