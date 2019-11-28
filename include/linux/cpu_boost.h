d18d52b99fa1396ef4dea88c1447c51f5a8bafac
ajaivasudeve <ajaivasudeve@gmail.com>
2019-11-28 09:11:31 +0530

    cpu-boost: implement max-boost functionality
    Co-authored-By: Francisco Franco <franciscofranco.1990@gmail.com>
    Co-authored-By: ajaivasudeve <ajaivasudeve@gmail.com>
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- include/linux/cpu_boost.h
+++ include/linux/cpu_boost.h
@@ -5,5 +5,10 @@
 
 #ifdef CONFIG_CPU_BOOST
 extern unsigned long last_input_time;
+void input_boost_max_kick(unsigned int duration_ms);
+#else
+static inline void input_boost_max_kick(unsigned int duration_ms)
+{
+}
 #endif
 #endif 
