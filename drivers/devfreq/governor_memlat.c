e59de9714a5668092949d124d43ea7411088b7de
dopaemon <polarisdp@gmail.com>
2020-07-06 10:30:20 +0000

    Revert "PM / devfreq: memlat: Look for min stall% in addition to ratio criteria"
    This reverts commit 5087a7ace102acdbc9ad7a7b7098c5d931069304.

--- drivers/devfreq/governor_memlat.c
+++ drivers/devfreq/governor_memlat.c
@@ -35,7 +35,6 @@
 
 struct memlat_node {
 	unsigned int ratio_ceil;
-	unsigned int stall_floor;
 	bool mon_started;
 	struct list_head list;
 	void *orig_data;
@@ -251,7 +250,6 @@ static int devfreq_memlat_get_freq(struct devfreq *df,
 			continue;
 
 		if (ratio <= node->ratio_ceil
-		    && hw->core_stats[i].stall_pct >= node->stall_floor
 		    && hw->core_stats[i].freq > max_freq) {
 			lat_dev = i;
 			max_freq = hw->core_stats[i].freq;
@@ -273,11 +271,9 @@ static int devfreq_memlat_get_freq(struct devfreq *df,
 }
 
 gov_attr(ratio_ceil, 1U, 10000U);
-gov_attr(stall_floor, 0U, 100U);
 
 static struct attribute *dev_attr[] = {
 	&dev_attr_ratio_ceil.attr,
-	&dev_attr_stall_floor.attr,
 	&dev_attr_freq_map.attr,
 	NULL,
 };
